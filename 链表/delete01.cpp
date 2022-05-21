a1db2a21189a68e5eefcadaebf39cfe6f2a0bd6a
Fuqianjiao <101001960+Fuqianjiao@users.noreply.github.com>
2022-05-21 09:32:39 +0800

    单链表对原结点进行删除

--- 链表/delete01.cpp
+++ 链表/delete01.cpp
@@ -1,33 +1,32 @@
-//直接使用原来的链表来进行移除节点操作：
+//直接使用原结点进行删除
 class Solution {
 public:
 ListNode*removeElements(ListNode*head,int val) {
     //删除头结点
-    while(head!=NULL&&head->val==val){
-        //tmp指针指向头结点
+    //判断头结点是否为空 头结点的元素是否相等
+    while(head!=NULL&&head->val==val) {
+        //tem指针指向头结点
         ListNode *tmp = head;
-        //头结点往后面移动一位
+        //将头结点往后面移一位
         head = head->next;
-        //删除原来头结点的内存空间
+        //删除头结点
         delete tmp;
     }
+    
     //删除非头结点
     //cur指针指向头结点
     ListNode *cur = head;
-    //头结点不为空以及头结点的后一个节点也不为空
+    //判断头结点是否为空 后一个元素指针域是否为空
     while(cur!=NULL&&cur->next!=NULL) {
-        //查找到了元素
+        //判断元素的值是否相等
         if(cur->next->val==val) {
-            //头结点的指针域指向tmp指针
+            //tmp的指针指向元素的指针域
             ListNode *tmp = cur->next;
-            //让节点next指针直接指向下下个节点
             cur->next = cur->next->next;
-            //删除tmp指针
             delete tmp;
-        } else {
-            //不进行删除 继续往后面遍历
-            cur = cur->next;
         }
+    } else {
+        cur = cur->next;
     }
     return head;
 }
